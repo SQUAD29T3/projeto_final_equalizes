8a7187ba27396aede87964829a7e1fce71089dd1
giovanesouza <developergiovanesouza@gmail.com>
2022-09-29 11:49:34 -0300

    Melhorias no código

--- js/login.js
+++ js/login.js
@@ -1,20 +1,30 @@
+// IMAGENS
+const imgPadrao = document.querySelector('.imgPadrao');
+const imgEscola = document.querySelector('.escola');
+const imgEmpresa = document.querySelector('.empresa');
+
+
+// OPTION
 const escolas = document.getElementById('escolas');
 const empresas = document.getElementById('empresas');
+
 
 escolas.addEventListener("change", escolha);
 empresas.addEventListener("change", escolha);
 
 function escolha() {
+
 	if (escolas.checked == true) {
-		
-		document.querySelector('.escola').style.display = "block";
-		document.querySelector('.empresa').style.display = "none";
-		document.querySelector('.img-padrao').style.display = "none";
+
+		imgEscola.style.display = "block";
+		imgEmpresa.style.display = "none";
+		imgPadrao.style.display = "none";
         
 	} else if (empresas.checked == true) {
-		document.querySelector('.escola').style.display = "none";
-		document.querySelector('.img-padrao').style.display = "none";
-		document.querySelector('.empresa').style.display = "block";
+		
+		imgEscola.style.display = "none";
+		imgPadrao.style.display = "none";
+		imgEmpresa.style.display = "block";
 
 	}
 }
