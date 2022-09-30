fe27e4d3686128c01e5b0fafe50c92c21a045804
m40r1 <99940252+m40r1@users.noreply.github.com>
2022-09-30 16:16:30 +0000

    esqueleto do controller da classe escola.

---
+++ MVC_Equalizes/src/main/java/main/equalizes_jsp/servlet/EscolaController.java
@@ -0,0 +1,75 @@
+package main.equalizes_jsp.servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Logger;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import main.equalizes_jsp.dao.DaoEscola;
+import main.equalizes_jsp.models.Escola;
+
+public class EscolaController extends HttpServlet {
+	private final DaoEscola escolaDao = DaoEscola.getInstance();
+	private static final Logger LOGGER = Logger.getLogger(EscolaController.class.getName());
+
+	@Override
+	protected void doPost(final HttpServletRequest req, final HttpServletResponse res)
+			throws ServletException, IOException {
+		doGet(req, res);
+	}
+
+	@Override
+	protected void doGet(final HttpServletRequest req, final HttpServletResponse res)
+			throws ServletException, IOException {
+		final String action = req.getServletPath();
+		// TODO implementar funcionalidade no site
+	}
+
+	private void updateEscola(final HttpServletRequest req, final HttpServletResponse res)
+			throws SQLException, IOException, ServletException {
+		final long escola_id = Long.parseLong(req.getParameter("escola_id"));
+		final int qt_alunos = Integer.parseInt("qt_alunos");
+
+		final Escola escola = new Escola(escola_id, qt_alunos);
+		escolaDao.update(escola);
+		// TODO redirect????
+		// fazer algo
+	}
+
+	private void deleteEscola(final HttpServletRequest req, final HttpServletResponse res)
+			throws SQLException, IOException, ServletException {
+		final long escola_id = Long.parseLong(req.getParameter("escola_id"));
+
+		final Escola escola = new Escola(escola_id);
+		escolaDao.delete(escola);
+	}
+
+	private void insertEscola(final HttpServletRequest req, final HttpServletResponse res)
+			throws SQLException, IOException, ServletException {
+		final long telefone = Long.parseLong(req.getParameter("telefone"));
+		final long cnpj = Long.parseLong(req.getParameter("cnpj"));
+		final long cep = Long.parseLong(req.getParameter("cep"));
+		final String nome = req.getParameter("nome");
+		final String cidade = req.getParameter("cidade");
+		final String uf = req.getParameter("uf");
+		final String email = req.getParameter("email");
+
+		final Escola escola = new Escola(cnpj, nome, uf, cep, cidade, email, telefone);
+		escolaDao.save(escola);
+	}
+
+	private void listEscola(final HttpServletRequest req, final HttpServletResponse res)
+			throws SQLException, IOException, ServletException {
+		final RequestDispatcher dispatcher = req.getRequestDispatcher("jsp/EscolaList.jsp");
+
+		final List<Escola> listEscola = escolaDao.findAll();
+		req.setAttribute("listEscola", listEscola);
+
+		dispatcher.forward(req, res);
+	}
+}
