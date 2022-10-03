98986b0fe7ee6ccbd8799299e28642f231be171e
m40r1 <99940252+m40r1@users.noreply.github.com>
2022-10-03 14:25:50 +0000

    Controller para projeto e interese

---
+++ MVC_Equalizes/src/main/java/main/equalizes_jsp/servlet/IntereseController.java
@@ -0,0 +1,60 @@
+package main.equalizes_jsp.servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import main.equalizes_jsp.dao.DaoInterese;
+import main.equalizes_jsp.models.Interese;
+
+public class IntereseController extends HttpServlet {
+	private final DaoInterese intereseDao = DaoInterese.getInstance();
+	private static final Logger LOGGER = Logger.getLogger(IntereseController.class.getName());
+
+	@Override
+	protected void doPost(final HttpServletRequest req, final HttpServletResponse res)
+			throws ServletException, IOException {
+		doGet(req, res);
+	}
+
+	@Override
+	protected void doGet(final HttpServletRequest req, final HttpServletResponse res)
+			throws ServletException, IOException {
+		final String action = req.getServletPath();
+		// TODO implementar funcionalidade no site
+	}
+
+	private void updateInterese(final HttpServletRequest req, final HttpServletResponse res)
+			throws SQLException, IOException, ServletException {
+		// TODO nome certo dos campos
+		final long interese_id = Long.parseLong(req.getParameter("interese_id"));
+		final String texto = req.getParameter("texto");
+
+		final Interese interese = new Interese(texto, interese_id);
+		intereseDao.update(interese);
+		// TODO redirect????
+		// fazer algo
+	}
+
+	private void deleteInterese(final HttpServletRequest req, final HttpServletResponse res)
+			throws SQLException, IOException, ServletException {
+		final long interese_id = Long.parseLong(req.getParameter("interese_id"));
+
+		final Interese interese = new Interese(interese_id);
+		intereseDao.delete(interese);
+	}
+
+	private void insertInterese(final HttpServletRequest req, final HttpServletResponse res)
+			throws SQLException, IOException, ServletException {
+		final String texto = req.getParameter("texto");
+		String imagem = req.getParameter("imagem ");
+		long perfil_id = Long.parseLong(req.getParameter("perfil_id"));
+		final Interese interese = new Interese(perfil_id, texto, imagem)
+		intereseDao.save(interese);
+	}
+
+}
---
+++ MVC_Equalizes/src/main/java/main/equalizes_jsp/servlet/ProjetoController.java
@@ -0,0 +1,61 @@
+package main.equalizes_jsp.servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import main.equalizes_jsp.dao.DaoProjeto;
+import main.equalizes_jsp.models.Projeto;
+
+public class ProjetoController extends HttpServlet {
+	private final DaoProjeto projetoDao = DaoProjeto.getInstance();
+	private static final Logger LOGGER = Logger.getLogger(ProjetoController.class.getName());
+
+	@Override
+	protected void doPost(final HttpServletRequest req, final HttpServletResponse res)
+			throws ServletException, IOException {
+		doGet(req, res);
+	}
+
+	@Override
+	protected void doGet(final HttpServletRequest req, final HttpServletResponse res)
+			throws ServletException, IOException {
+		final String action = req.getServletPath();
+		// TODO implementar funcionalidade no site
+	}
+
+	private void updateProjeto(final HttpServletRequest req, final HttpServletResponse res)
+			throws SQLException, IOException, ServletException {
+		// TODO nome certo dos campos
+		final long projeto_id = Long.parseLong(req.getParameter("projeto_id"));
+		final String estado = req.getParameter("estado");
+
+		final Projeto projeto = new Projeto(projeto_id, estado);
+		projetoDao.update(projeto);
+		// TODO redirect????
+		// fazer algo
+	}
+
+	private void deleteProjeto(final HttpServletRequest req, final HttpServletResponse res)
+			throws SQLException, IOException, ServletException {
+		final long projeto_id = Long.parseLong(req.getParameter("projeto_id"));
+
+		final Projeto projeto = new Projeto(projeto_id);
+		projetoDao.delete(projeto);
+	}
+
+	private void insertProjeto(final HttpServletRequest req, final HttpServletResponse res)
+			throws SQLException, IOException, ServletException {
+		String estado = req.getParameter("estado");
+		long id_perfil_escola = Long.parseLong(req.getParameter("id_perfil_escola"));
+		long id_perfil_empresa = Long.parseLong(req.getParameter("id_perfil_empresa"));
+		long id_interese = Long.parseLong(req.getParameter("id_interese"));
+		final Projeto projeto = new Projeto(id_interese, id_perfil_escola, id_perfil_empresa, estado);
+		projetoDao.save(projeto);
+	}
+
+}
